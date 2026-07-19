a0c1ae75f0bf8cc6a51d284c8937f4461f598e12
davdev12 <davbot.github@gmail.com>
2026-07-19 19:17:24 +0200

    Adds Helvetica Font

--- game.py
+++ game.py
@@ -17,6 +17,12 @@ game_surface = pygame.Surface((WIDTH, HEIGHT))
 
 skala = pygame.image.load("images/gradskala.png").convert_alpha()
 periskopL = pygame.image.load("images/periskop.png").convert_alpha()
+background = pygame.image.load("images/bg.png").convert_alpha()
+mapl = pygame.image.load("images/map.png").convert_alpha()
+
+HelveticaNM = pygame.font.Font("fonts/HelveticaNeueMedium.ttf", 30)
+
+background_width = background.get_width()
 skala_width = skala.get_width()
 
 scale = 0.45  #45%
@@ -26,27 +32,45 @@ new_height = int(periskopL.get_height() * scale)
 
 periskop = pygame.transform.scale(periskopL, (new_width, new_height))
 
-offset = 613
-degree = 0
-goal_degree = randint(0, 3)
+scale = 0.43
 
+new_width = int(mapl.get_width() * scale)
+new_height = int(mapl.get_height() * scale)
+
+map = pygame.transform.scale(mapl, (new_width, new_height))
+
+dist_a = 100
+dist_b = 100
+dist_c = 100
+
+offset = 613
+bg_offset = 613
+
+game_state = 0
+
+degree = 0
+goal_degree = randint(0, 36) * 10
+backroundx = randint(0, 900)
 enemy_rect = pygame.Rect((300, 250, 200, 50))
 
 pygame.display.set_caption("Sea Raider 2")
 clock = pygame.time.Clock()
 
-game_state = 0
+dist_a_surf = HelveticaNM.render(str(dist_a), False, (255, 255, 255))
+dist_b_surf = HelveticaNM.render(str(f"{dist_b}"+"m"), False, (255, 255, 255))
+dist_c_surf = HelveticaNM.render(str(dist_c), False, (255, 255, 255))
+
 
 
 
 def keyboard():
-    global offset, degree
+    global offset, degree, bg_offset
     keys = pygame.key.get_pressed()
 
     if keys[pygame.K_w]:
         pass
 
-    if keys[pygame.K_a]:
+    if keys[pygame.K_a] and game_state == 1:
         offset -= 3
         if degree < 0:
             degree = 360
@@ -55,20 +79,30 @@ def keyboard():
     if keys[pygame.K_s]:
         pass
 
-    if keys[pygame.K_d]:
+    if keys[pygame.K_d] and game_state == 1:
         offset += 3
         if degree > 360:
             degree = 0
         degree += 0.5
 
     offset %= skala_width
+    bg_offset %= background_width
 def draw():
 
 
     game_surface.fill((255, 255, 255))
-    game_surface.blit(skala, (-offset, -300))
-    game_surface.blit(skala, (skala_width - offset, -300))
-    game_surface.blit(periskop, (-35, -10))
+    if game_state == 0:
+        game_surface.blit(map, (0, 0))
+        game_surface.blit(dist_b_surf, (160, 265))
+
+
+
+    elif game_state == 1:
+        game_surface.blit(background, (-offset + backroundx, -300))
+        game_surface.blit(background, (background_width - offset + backroundx, -300))
+        game_surface.blit(skala, (-offset, -300))
+        game_surface.blit(skala, (skala_width - offset, -300))
+        game_surface.blit(periskop, (-35, -10))
 
 while True:
 
@@ -86,21 +120,31 @@ while True:
 
         elif event.type == pygame.KEYDOWN:
 
-            if event.key == pygame.K_ESCAPE:
+            if event.key == pygame.KMOD_ALT and event.key == pygame.K_F4:
                 pygame.quit()
                 exit()
 
             elif event.key == pygame.K_r and pygame.key.get_mods() & pygame.KMOD_CTRL:
                 pass
 
+            elif event.key == pygame.K_ESCAPE:
+                game_state -= 1
+
             elif event.key == pygame.K_SPACE:
-                pass
+                if game_state == 0:
+                    game_state = 1
+                elif game_state == 1 and goal_degree - 5 < degree < goal_degree + 5:
+                    game_state = 2
+
 
 
 
     keyboard()
     draw()
-    print(int(degree), "°")
+    if game_state == 1:
+        print(int(degree), "°")
+    elif game_state == 0:
+        print(goal_degree)
     #pygame.draw.rect(game_surface, (255, 0, 0), enemy_rect)
 
 
